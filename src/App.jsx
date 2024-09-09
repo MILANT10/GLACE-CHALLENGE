c6911fe5cac9d6142183fec99bef8b43448624b4
Milan TURCANO <turcano.milan@sfr.fr>
2024-09-09 10:37:39 +0200

    Première version du titre sur la gauche avec le bouton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Title from "./components/Title/Title";
 
 export default function App() {
   return (
-    <main className="size-full bg-darkBrown py-5 px-40 font-recoleta">
+    <main className="w-full h-fit py-5 px-40 font-recoleta">
       <Header />
+      <section className="size-full mt-16">
+        <Title />
+      </section>
     </main>
   );
 }
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,16 @@
+export default function Title() {
+  return (
+    <aside className="w-1/2 flex flex-col items-start gap-8 font-recoleta">
+      <h2 className="text-9xl text-white w-full">
+        Grab the best ice cream. <span className="text-7xl">🍧</span>
+      </h2>
+      <p className="text-xl text-white w-full">
+        We ditched the dairy we cut the sugar infused <br /> mood-boosting
+        adaptogens
+      </p>
+      <button className="px-14 py-8 bg-honey ovale text-2xl border-b-2 border-black">
+        Shop Flavors
+      </button>
+    </aside>
+  );
+}
