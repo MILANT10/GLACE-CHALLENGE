8868f57cc3a664a42990765cdad33ca2fc9d6a80
Milan TURCANO <turcano.milan@sfr.fr>
2024-09-09 11:28:08 +0200

    Première version de l'image avec la glace

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,17 @@ import Title from "./components/Title/Title";
 
 export default function App() {
   return (
-    <main className="w-full h-fit py-5 px-40 font-recoleta">
+    <main className="w-full py-5 px-40 font-recoleta">
       <Header />
-      <section className="size-full mt-16">
+      <section className="size-full mt-16 flex items-center">
         <Title />
+        <section className="w-1/2 flex items-center justify-center">
+          <img
+            className="size-[34rem] object-contain"
+            src="/ice-cream.webp"
+            alt="ice-cream photo"
+          />
+        </section>
       </section>
     </main>
   );
