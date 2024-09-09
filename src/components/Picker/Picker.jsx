eca8d6e4b506465fcf6c3e3480a6494df375ded2
Milan TURCANO <turcano.milan@sfr.fr>
2024-09-09 13:48:46 +0200

    Première version du picker

--- src/components/Picker/Picker.jsx
+++ src/components/Picker/Picker.jsx
@@ -1,6 +1,29 @@
 export default function Picker() {
+  const BUTTONS = [
+    { title: "Chocolate gelato", color: "bg-amber-100" },
+    { title: "Strawberry gelato", color: "bg-red-100" },
+    { title: "Mint gelato", color: "bg-green-100" },
+    { title: "Vanilla gelato", color: "bg-blue-100" },
+    { title: "Pistachio gelato", color: "bg-green-400" },
+    { title: "Lemon gelato", color: "bg-yellow-100" },
+    { title: "Coffee gelato", color: "bg-amber-400" },
+  ];
+
   return (
-    <section className="w-full h-full relative overflow-x-hidden pt-6">
+    <section className="w-full h-full overflow-x-hidden pt-6 relative bg-">
+      <section className="circle">
+        {BUTTONS.map((button, index) => (
+          <button
+            key={index}
+            className={
+              "bg-blue-100 text-black border border-black px-4 py-2 rounded-full " +
+              button.color
+            }
+          >
+            {button.title}
+          </button>
+        ))}
+      </section>
       <img
         className="w-full bg-darkBrown scale-11O"
         src="/rounded.svg"
