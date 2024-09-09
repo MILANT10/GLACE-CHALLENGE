a931d302ce9ea6cbd3561712529505a8ced7328c
Milan TURCANO <turcano.milan@sfr.fr>
2024-09-09 09:29:18 +0200

    Ajout de couleur de base dans la config et de la police qui semble être la bonne

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBrown: "#361a0c",
+        honey: "#f5bf07",
+      },
+      fontFamily: {
+        recoleta: ["Recoleta"],
+      },
+    },
+    colors: {
+      black: "#140200",
+    },
   },
   plugins: [],
 };
